ec5e028d981ea4c9c0112d4733a73c88bf024f91
Benjamin Gudehus <hastebrot@gmail.com>
2011-12-12 16:14:38 +0100

    Added; compiler instruction for architecture detection.

--- trunk/native/src/DdeAPI.cpp
+++ trunk/native/src/DdeAPI.cpp
@@ -90,7 +90,12 @@ JNIEXPORT jint JNICALL Java_com_google_code_jdde_ddeml_DdeAPI_Connect
 	UtilFreeStringHandle(idInst, hszService);
 	UtilFreeStringHandle(idInst, hszTopic);
 
-	return (UINT64) hConv;
+  #ifdef __i386__
+    return (UINT) hConv;
+  #endif
+  #ifdef __x86_64__
+    return (UINT64) hConv;
+  #endif
 }
 
 JNIEXPORT jboolean JNICALL Java_com_google_code_jdde_ddeml_DdeAPI_Disconnect
